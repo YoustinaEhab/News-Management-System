2113bd490838c74a18bb2fdf014f078af6b5a32f
YoustinaEhab <98767031+YoustinaEhab@users.noreply.github.com>
2023-11-17 10:31:23 -0800

    Add files via upload

---
+++ News_Managment_System/adminmainwindow.h
@@ -0,0 +1,33 @@
+#ifndef ADMINMAINWINDOW_H
+#define ADMINMAINWINDOW_H
+
+#include "categorymainwindow.h"
+#include "newsadminwindow.h"
+#include <QDialog>
+class MainWindow;
+
+namespace Ui {
+class AdminMainWindow;
+}
+
+class AdminMainWindow : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit AdminMainWindow(QWidget *parent = nullptr);
+    ~AdminMainWindow();
+
+private slots:
+    void on_CategoryButton_clicked();
+    void on_NewsButton_clicked();
+    void on_log_out_clicked();
+
+private:
+    Ui::AdminMainWindow *ui;
+    CategoryMainWindow *category;
+    NewsAdminWindow *news;
+    MainWindow* mainwindow;
+};
+
+#endif // ADMINMAINWINDOW_H
